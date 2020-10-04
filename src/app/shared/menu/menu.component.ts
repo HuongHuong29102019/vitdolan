986f30d108ea6a992e6d9b7e2a4a58e84f909137
HuongHuong29102019 <70678478+HuongHuong29102019@users.noreply.github.com>
2020-10-05 00:01:38 +0700

    update code

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -10,6 +10,8 @@ import { BaseComponent } from '../../lib/base-component';
 export class MenuComponent extends BaseComponent implements OnInit {
   menus:any;
   total:any;
+  items:any;
+  money:any;
   constructor(injector: Injector) { 
     super(injector);
   }
@@ -20,6 +22,13 @@ export class MenuComponent extends BaseComponent implements OnInit {
     this._cart.items.subscribe((res) => {
       this.total = res? res.length:0;
     });
+    this._cart.items.subscribe((res) => {
+      this.items = res;
+      this.money = 0;
+      for(let x of this.items){ 
+        x.moneysum = x.quantity * x.item_price;
+        this.money += x.quantity * x.item_price;
+      } 
+    });
   }
-
 }
